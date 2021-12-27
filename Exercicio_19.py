88a44d221d901a5cb732b0e16a2e7f1aabbb4130
Pedro Bonfim <Phenrique646@gmail.com>
2021-12-27 19:15:26 -0300

    Atidades realizadas

---
+++ Exercicio_19.py
@@ -0,0 +1,3 @@
+Litros=float(input("Digite o volume em Litros que será convertido para Metros Cúbicos: "))
+Metros_cúbicos= (Litros/1000)
+print(f"O volume em Metros Cúbicos é igual a: {Metros_cúbicos} Metros Cúbicos. ")
