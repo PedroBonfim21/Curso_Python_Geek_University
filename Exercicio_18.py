161eec158b2809a5e1699711a9004a933e194f7e
Pedro Bonfim <Phenrique646@gmail.com>
2021-12-27 19:14:50 -0300

    Atidades realizadas

---
+++ Exercicio_18.py
@@ -0,0 +1,3 @@
+Metros_cúbicos=float(input("Digite o volume em metros cúbicos que será convertido para litros: "))
+litros= (1000*Metros_cúbicos)
+print(f"O volume em litros é igual a: {litros}L. ")
