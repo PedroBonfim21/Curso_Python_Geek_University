d1e9a43508d5ea352ec4abfe04ca576ed812e094
Pedro Bonfim <Phenrique646@gmail.com>
2021-12-27 19:14:01 -0300

    Atidades realizadas

---
+++ Exercicio_17.py
@@ -0,0 +1,3 @@
+centímetros=float(input("Digite um comprimento em Centímetros que sera convertido em Polegadas: "))
+polegadas= (centímetros/2.54)
+print(f"O comprimento em Polegadas é igual a: {polegadas} .")
