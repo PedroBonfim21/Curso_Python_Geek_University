977eabe9b7645f059436755184dc58799018ae94
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-01 04:27:56 -0300

    Atividades realizadas

---
+++ Exercicio_29.py
@@ -0,0 +1,6 @@
+nota1=float(input("primeira nota: "))
+nota2=float(input("segunda nota: "))
+nota3=float(input("terceira nota: "))
+nota4=float(input("quarta nota: "))
+Média_aritmética= (nota1 + nota2 + nota3 + nota4)/4
+print(f"A média é: {Média_aritmética}.") 
