ab6cf2d43718a54a61c9ee8be37ffbe3afc5efc6
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-01 04:32:53 -0300

    Atividades realizadas

---
+++ Exercicio_31.py
@@ -0,0 +1,5 @@
+número_inteiro=int(input("Digite um valor inteiro: "))
+antecessor= (número_inteiro - 1)
+sucessor= (número_inteiro + 1)
+print(f"O antecessor é: {antecessor}.")
+print(f"O sucessor é: {sucessor}.")
