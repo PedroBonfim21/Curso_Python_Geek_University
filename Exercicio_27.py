9de55e58f35b715512ff4fe8adb4676bc73b3a03
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-01 04:26:35 -0300

    Atividades realizadas

---
+++ Exercicio_27.py
@@ -0,0 +1,3 @@
+hectares= float(input("Digite o valor em hectares para ser convertido em metros quadrados: "))
+metros2= (hectares * 10000)
+print(f"O valor convertido em metros quadrados é igual a: {metros2}")
