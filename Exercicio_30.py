4936da9dc009506bf522cc7087add683255abf97
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-01 04:31:58 -0300

    Atividades realizadas

---
+++ Exercicio_30.py
@@ -0,0 +1,4 @@
+real=float(input("o valor em real é: "))
+cotação_dólar=float(input("cotação do dólar: "))
+valor_em_dólar= (real * cotação_dólar)
+print(f"o valor correspondente em dólar é: {valor_em_dólar}")
