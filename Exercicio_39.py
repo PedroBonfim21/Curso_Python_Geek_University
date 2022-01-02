b14c7a003ce040819f83665bd48a37a2a8a32459
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-02 04:50:08 -0300

    Atividades realizadas

---
+++ Exercicio_39.py
@@ -0,0 +1,8 @@
+valor=780000
+primeiro=valor*(46/100)
+segundo=valor*(32/100)
+terceiro=valor*(22/100)
+print(f"O valor do premio é: {valor}.")
+print(f"O valor que o primeiro irá ganhar é de: {primeiro} .")
+print(f"O valor que o segundo irá ganhar é de: {segundo} .")
+print("O valor que o terceiro irá ganhar é de: {:.1f} .".format(terceiro))
