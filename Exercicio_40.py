d08b357a7655224905ce008a9fcc1ddb46fd2a66
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-02 04:50:48 -0300

    Atividades realizadas

---
+++ Exercicio_40.py
@@ -0,0 +1,5 @@
+dias=int(input("Quantos dias o encanador trabalhou: "))
+quantia=dias*30
+liquido=(8*quantia)/100
+quantia_liquida=quantia-liquido
+print(f"a quantia liquida é de: {quantia_liquida} .")
