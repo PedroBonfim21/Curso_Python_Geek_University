87692f03e4c4186d4731c6bd324ef2b219905cf9
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-02 04:47:55 -0300

    Atividades realizadas

---
+++ Exercicio_36.py
@@ -0,0 +1,4 @@
+altura=float(input("Digite a altura do cilindro: "))
+raio=float(input("Digite o raio do cilindro: "))
+pi=3.141592;volume=(pi*(raio**2)*altura)
+print(f"i volume do cilindro é igual a: {volume} .")
