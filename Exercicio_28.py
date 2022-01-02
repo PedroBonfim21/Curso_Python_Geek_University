75d069b20d6101f54cd64098a50fc6a3e7e5c1cb
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-02 00:50:20 -0300

    Atividades realizadas

--- Exercicio_28.py
+++ Exercicio_28.py
@@ -2,5 +2,5 @@ valor1= float(input("Digite o primeiro valor: "))
 valor2= float(input("Digite o segundo valor:  "))
 valor3= float(input("Digite o terceiro valor: "))
 
-resultado=(( valor1 * valor1 ) + ( valor2 * valor2 ) +( valor3 * valor3 ))
+resultado=(( valor1**2 ) + ( valor2**2 ) +( valor3**2 ))
 print(f"a soma dos quadrados dos 3 valores é igual a: {resultado}.")
