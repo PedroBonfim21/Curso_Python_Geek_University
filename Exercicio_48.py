8a91a5ff26149a380a6275456bc310a35cf73e89
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 12:52:59 -0300

    Atividades realizadas

---
+++ Exercicio_48.py
@@ -0,0 +1,6 @@
+segundos=int(input("Digite um valor em segundos: "))
+horas= segundos/3600
+minutos= segundos/60
+print(f"O valor de segundos em horas é: {horas} ")
+print(f"O valor de segundos em minutos é: {minutos} ")
+print(f"O valor de segundos em segundos é: {segundos} ")
