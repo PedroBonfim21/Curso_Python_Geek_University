6708e37ba6fcdf8313133a2c9e48aa8a0b2ce049
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 12:49:47 -0300

    Atividades realizadas

---
+++ Exercicio_44.py
@@ -0,0 +1,4 @@
+degrau=float(input("Qual a altura do degrau: "))
+altura=float(input("Qual a altura que você quer chegar: "))
+quantidade= altura/degrau
+print(f"Você deve subir {quantidade} degraus para chegar até a altura desejada.")
