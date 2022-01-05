bf586c50081a8f71dae62df601f11aff81a6580e
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 12:52:04 -0300

    Atividades realizadas

---
+++ Exercicio_47.py
@@ -0,0 +1,6 @@
+numero=int(input("Digite um numero inteiro de 4digitos (de 1000 a 9999): "))
+linhas=str(numero)
+print(str(linhas[0]))
+print(str(linhas[1]))
+print(str(linhas[2]))
+print(str(linhas[3]))
