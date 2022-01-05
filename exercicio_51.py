acbad3eab3f6971bb14795e91f2e17e62b530cb6
Pedro Bonfim <Phenrique646@gmail.com>
2022-01-05 12:54:26 -0300

    Atividades realizadas

---
+++ exercicio_51.py
@@ -0,0 +1,5 @@
+coordenadax=float(input("Digite a coordenada X: "))
+coordenaday=float(input("Digite a coordenada Y: "))
+distanciax= coordenadax-0
+distanciay= coordenaday-0
+print(f"A distância da origem é igual a: ({distanciax},{distanciay}) ") 
